e8871faf2745211c9e166d6993153ebd1587d6d1
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-06-01 11:12:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,38 +38,34 @@ welcomeMessage() {
 
 branch=master
 gp-package=GlobalProtect_deb_arm-5.1.2.0-26.deb
+gp-ca-cert=cert_GP-DuckDNS-CA.crt
+gp-cert=cert_GP-DuckDNS.crt
+gp-config=pangps.xml
 
 getFiles() {
 	mkdir gp
 	cd gp
 	wget -q https://raw.githubusercontent.com/KnoAll/globalprotect-iot/$branch/$gp-package
-	wget -q https://raw.githubusercontent.com/KnoAll/globalprotect-iot/$branch/cert_GP-DuckDNS-CA.crt
-	wget -q https://raw.githubusercontent.com/KnoAll/globalprotect-iot/$branch/cert_GP-DuckDNS.crt
+	wget -q https://raw.githubusercontent.com/KnoAll/globalprotect-iot/$branch/$gp-ca-cert
+	wget -q https://raw.githubusercontent.com/KnoAll/globalprotect-iot/$branch/$gp-cert
+	wget -q https://raw.githubusercontent.com/KnoAll/globalprotect-iot/$branch/$gp-config
 }
 
 setupInstall() {
 	sudo dpkg -i $gp-package
+	sudo cp pangps.xml /opt/paloaltonetworks/globalprotect/
 }
+
+setupRemove() {
+	sudo dpkg -P globalprotect
+}
+
 
 welcomeLooper() {
 case $1 in
 	dev)
 		printwarn "Switching to DEV script"
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/upgrade-cacti.sh) $1 $2
-	;;
-	--fix-permissions)
-		welcomeMessage
-		counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=update-permissions&write=0 )
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/$branch/update-permissions.sh) $1 $2
-	;;
-	--backup-data)
-		welcomeMessage
-		counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=backup-data&write=0 )
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/$branch/backup-cacti.sh) $1 $2
-	;;
-	--restore-data)
-		welcomeMessage
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/$branch/restore-cacti.sh) $1 $2
+		bash <(curl -s https://raw.githubusercontent.com/KnoAll/globalprotect-iot/dev/setup.sh) $1 $2
 	;;
 	--switch-dev)
 		printwarn "Switching to DEV branch"
@@ -78,9 +74,6 @@ case $1 in
 	;;
 	--help | --h | --H | help | -? | --? )
 		printinfo "Switches available in this script:"
-		printinfo "--backup-data	Backup Cacti data (only) and compress to home directory"
-		printinfo "--fix-permissions	Fix file permissions if graphs not created properly"
-		printinfo "--restore-data	Restore Cacti from previous backup"
 	;;
 	*)
 		welcomeMessage
